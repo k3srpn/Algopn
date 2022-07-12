8d7f602ed53393b388429cee4cf4f8c9dbebc940
srpn <102716982+k3srpn@users.noreply.github.com>
2022-07-12 17:25:20 +0900

    [Silver IV] Title: 한수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,23 @@
+X = input()
+
+def hansu(a: list) -> int:
+    cnt = 0
+    arr_length = len(a)
+    if arr_length == 1:
+        return 0
+    front_cd = a[0] - a[1]
+    end_cd = a[1] - a[2]
+    if front_cd == end_cd:
+        cnt += 1
+    return cnt
+
+num = 0
+for i in range(1, int(X)+1):
+    if i < 100 : 
+        num += 1
+    else:
+        X_arr = [int(j) for j in str(i)]
+        num += hansu(X_arr)
+
+print(num)
+    
