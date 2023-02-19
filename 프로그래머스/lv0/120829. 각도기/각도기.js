e1d968f89f70977d8ff9b8b5104d50b182315d3b
srpn <102716982+k3srpn@users.noreply.github.com>
2023-02-19 22:07:41 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,7 @@
+function solution(angle) {
+    let answer = 0
+    if(angle < 90 && angle > 0) return 1 
+    else if(angle === 90) return 2
+    else if(angle > 90 && angle < 180) return 3
+    else if(angle === 180) return 4
+}
