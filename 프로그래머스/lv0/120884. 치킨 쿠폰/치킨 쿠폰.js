be2ec6231906b94942f45dff1a6118ac6b55c894
srpn <102716982+k3srpn@users.noreply.github.com>
2023-03-10 22:12:26 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+    let coupon = chicken
+    let service = 0
+    while (coupon >= 10) {
+        service += ~~(coupon/10)
+        coupon = ~~(coupon/10) + coupon % 10
+    }
+    return  service
+}
